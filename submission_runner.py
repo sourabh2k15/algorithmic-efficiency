ac2626bc6db2ba7b5491051ae45b22b23b04cc09
pomonam <juhan.bae@mail.utoronto.ca>
2023-07-27 02:57:36 -0400

    minor

--- submission_runner.py
+++ submission_runner.py
@@ -214,7 +214,7 @@ def train_once(
       #   logging.info('Performing `torch.compile`.')
       #   model_params = torch.compile(model_params)
 
-      model_params = torch.compile(model_params, fullgraph=False)
+      model_params = torch.compile(model_params, fullgraph=False, backend="aot_eager")
 
   logging.info('Initializing optimizer.')
   with profiler.profile('Initializing optimizer'):
